eaa5adf95faf7613a4ecb22bbaa2af7d0436d812
maaksa <milosmilos@ymail.com>
2020-12-21 20:08:51 +0100

    Initial commit

---
+++ src/test/java/sk_microservices/DiscoveryService/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk_microservices.DiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
